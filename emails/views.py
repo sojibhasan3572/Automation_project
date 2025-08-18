0a0e8b2ac80c7d1abb73521850f9bd3a3addba8e
phitron-demo <sojibhasan5800@gmail.com>
2025-08-18 08:49:19 +0600

    Add robust email sending feature with Celery, dynamic subscriber handling, and attachment support

--- emails/views.py
+++ emails/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from .tasks import send_email_task
+from .forms import EmailForm
+from django.contrib import messages
+from dataentry.utlis import send_email_notification
+from django.conf import settings
+from .models import Email, Sent, Subscriber, EmailTracking
+from django.db.models import Sum
+from django.utils import timezone
+
+
+def send_email(request):
+    if request.method == 'POST':
+        email_form = EmailForm(request.POST, request.FILES)
+        if email_form.is_valid():
+            email = email_form.save()
+            # Send an email
+            mail_subject = request.POST.get('subject')
+            message = request.POST.get('body')
+            email_list = request.POST.get('email_list')
+            
+            # Access the selected email list
+            email_list = email.email_list
+            
+            # Extract email addresses from the Subscriber model in the selected email list
+            subscribers = Subscriber.objects.filter(email_list=email_list)
+
+            to_email = [email.email_address for email in subscribers]
+
+            if email.attachment:
+                attachment = email.attachment.path
+            else:
+                attachment = None
+            
+            email_id = email.id
+
+            # Handover email sending task to celery
+            send_email_task.delay(mail_subject, message, to_email, attachment, email_id)
+
+            # Display a success message
+            messages.success(request, 'Email sent successfully!')
+            return redirect('send_email')
+    else:
+        email = EmailForm()
+        context = {
+            'email_form': email,
+        }
+        return render(request, 'emails/send-email.html', context)
