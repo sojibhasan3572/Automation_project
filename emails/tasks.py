afc0cd9f504613ca34d673f1a61e1b982b6ae1b2
phitron-demo <sojibhasan5800@gmail.com>
2025-08-18 13:05:10 +0600

    Implement asynchronous email sending task with Celery
    - Added a Celery task `send_email_task` to handle email notifications asynchronously.
    - Integrates with `send_email_notification` utility for sending emails with attachments.
    - Ensures reliable and non-blocking email processing for improved app performance.

---
+++ emails/tasks.py
@@ -0,0 +1,8 @@
+from automation_dj.celery import app
+from dataentry.utlis import send_email_notification
+
+
+@app.task
+def send_email_task(mail_subject, message, to_email, attachment, email_id):
+    send_email_notification(mail_subject, message, to_email, attachment, email_id)
+    return 'Email sending task executed successfully.'
