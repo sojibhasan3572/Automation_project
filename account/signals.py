bd8071438afe059d8cc954da642b939f6b45402b
phitron-demo <sojibhasan5800@gmail.com>
2025-08-20 08:03:51 +0600

    Add secure 2-minute, time-boxed email OTP verification with expiry enforcement, single-use tokens, and graceful UX

--- account/signals.py
+++ account/signals.py
@@ -4,7 +4,8 @@ from django.dispatch import receiver
 from .models import OtpToken
 from django.core.mail import send_mail
 from django.utils import timezone
-
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
  
  
 @receiver(post_save, sender=settings.AUTH_USER_MODEL) 
@@ -14,33 +15,33 @@ def create_token(sender, instance, created, **kwargs):
             pass
         
         else:
-            OtpToken.objects.create(user=instance, otp_expires_at=timezone.now() + timezone.timedelta(minutes=5))
+            OtpToken.objects.create(user=instance, otp_expires_at=timezone.now() + timezone.timedelta(minutes=2))
             instance.is_active=False 
             instance.save()
         
         
         # email credentials
         otp = OtpToken.objects.filter(user=instance).last()
-       
-       
-        subject="Email Verification"
-        message = f"""
-                                Hi {instance.username}, here is your OTP {otp.otp_code} 
-                                it expires in 5 minute, use the url below to redirect back to the website
-                                http://127.0.0.1:8000/verify-email/{instance.username}
-                                
-                                """
-        sender = settings.EMAIL_HOST_USER
-        receiver = [instance.email, ]
+
+        # context data
+        context = {
+            "username": instance.username,
+            "otp": otp.otp_code,
+            "verify_url": f"http://127.0.0.1:8000/verify-email/{instance.username}",
+        }
        
         
         
-        # send email
-        send_mail(
-                subject,
-                message,
-                sender,
-                receiver,
-                fail_silently=False,
-            )
+        # HTML Template render
+        subject = "Verify Your Email Address"
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [instance.email]
+
+        text_content = render_to_string("accounts/otp_email.txt", context)  # fallback text
+        html_content = render_to_string("accounts/otp_email.html", context)  # beautiful design
+
+        # Email send
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to_email)
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
   
--- account/views.py
+++ account/views.py
@@ -8,6 +8,8 @@ from .models import OtpToken
 from django.utils import timezone
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -64,30 +66,31 @@ def resend_otp(request):
         
         if get_user_model().objects.filter(email=user_email).exists():
             user = get_user_model().objects.get(email=user_email)
-            otp = OtpToken.objects.create(user=user, otp_expires_at=timezone.now() + timezone.timedelta(minutes=5))
+            otp = OtpToken.objects.create(user=user, otp_expires_at=timezone.now() + timezone.timedelta(minutes=2))
             
             
-            # email variables
-            subject="Email Verification"
-            message = f"""
-                                Hi {user.username}, here is your OTP {otp.otp_code} 
-                                it expires in 5 minute, use the url below to redirect back to the website
-                                http://127.0.0.1:8000/verify-email/{user.username}
-                                
-                                """
-            sender = settings.EMAIL_HOST_USER
-            receiver = [user.email, ]
-        
-        
-            # send email
-            send_mail(
-                    subject,
-                    message,
-                    sender,
-                    receiver,
-                    fail_silently=False,
-                )
+            # context data
+            context = {
+                "username": user.username,
+                "otp": otp.otp_code,
+                "verify_url": f"http://127.0.0.1:8000/verify-email/{user.username}",
+            }
             
+            
+            
+            # HTML Template render
+            subject = "Verify Your Email Address"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = [user.email]
+
+            text_content = render_to_string("accounts/otp_email.txt", context)  # fallback text
+            html_content = render_to_string("accounts/otp_email.html", context)  # beautiful design
+
+            # Email send
+            msg = EmailMultiAlternatives(subject, text_content, from_email, to_email)
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+                
             messages.success(request, "A new OTP has been sent to your email-address")
             return redirect("verify-email", username=user.username)
 
