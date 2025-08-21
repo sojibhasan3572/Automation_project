b77d7acd4c4233dc6641e24efe64266a10aeff40
phitron-demo <sojibhasan5800@gmail.com>
2025-08-21 11:50:23 +0600

    feat: add CompressImage model, register in admin, and run migrations for image compression functionality

--- image_compression/models.py
+++ image_compression/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+class CompressImage(models.Model):
+    # [(10, 10), (20, 20), (30, 30), .... (100, 100)]
+    QUALITY_CHOICES = [(i, i) for i in range(10, 101, 10)]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    original_img = models.ImageField(upload_to='original_images/')
+    quality = models.IntegerField(choices=QUALITY_CHOICES, default=80)
+    compressed_img = models.ImageField(upload_to='compressed_images/')
+    compressed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
