541759e83e4a19a3f048badf13c6c48071246746
phitron-demo <sojibhasan5800@gmail.com>
2025-08-22 09:21:49 +0600

    Implement stock analysis form with crispy-forms and django-autocomplete-light integration in stock.html

---
+++ stockanalysis/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('stocks/', views.stocks, name='stocks'),
+    path('stock-autocomplete/', views.StockAutocomplete.as_view(), name="stock_autocomplete"),
+]
--- stockanalysis/views.py
+++ stockanalysis/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from dal import autocomplete
+from .models import Stock
 
 # Create your views here.
+
+def stocks(request):
+    return render(request, 'stockanalysis/stocks.html')
+
+
+class StockAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = Stock.objects.all()
+        
+        if self.q:
+            print('entered keyword=>', self.q)
+            qs = qs.filter(name__istartswith=self.q)
+            print('result==>', qs)
+
+        return qs
+    
