ddb0b9fc7aef20d4fc1f72302fb7f9877cff88ff
phitron-demo <sojibhasan5800@gmail.com>
2025-08-22 08:44:40 +0600

    Add Stock model with fields for symbol, sector, exchange, and country, and apply initial migrations

--- stockanalysis/models.py
+++ stockanalysis/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model):
+    name = models.CharField(max_length=255)
+    symbol = models.CharField(max_length=50) # Unique stock symbol
+    sector = models.CharField(max_length=100, null=True, blank=True)
+    exchange = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
