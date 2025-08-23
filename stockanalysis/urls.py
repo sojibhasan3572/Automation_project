67cb969d29f2539182c9208a5249a3148ff0a7be
phitron-demo <sojibhasan5800@gmail.com>
2025-08-23 08:22:04 +0600

    Implement Yahoo Finance scraper and integrate stock data update in stocks view

--- stockanalysis/urls.py
+++ stockanalysis/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('stocks/', views.stocks, name='stocks'),
     path('stock-autocomplete/', views.StockAutocomplete.as_view(), name="stock_autocomplete"),
+    path('stock-detail/<int:pk>/', views.stock_detail, name='stock_detail'),
 ]
--- stockanalysis/views.py
+++ stockanalysis/views.py
@@ -1,17 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from dal import autocomplete
-from .models import Stock
+from .models import Stock,StockData
 from .forms import StockForm
+from .utils import scrape_stock_data
+from django.contrib import messages
 
 # Create your views here.
 
 def stocks(request):
-    form = StockForm()
-    context = {
+    if request.method == 'POST':
+        form = StockForm(request.POST)
+        if form.is_valid():
+            stock_id = request.POST.get('stock')
+            # fetch the stock and symbol
+            stock = Stock.objects.get(pk=stock_id)
+            symbol = stock.symbol
+            exchange = stock.exchange
+            stock_response = scrape_stock_data(symbol, exchange)
+            
+            if stock_response:
+                try:
+                    stock_data = StockData.objects.get(stock=stock)
+                except StockData.DoesNotExist:
+                    stock_data = StockData(stock=stock)
+
+                # update the StockData instance with the response data
+                stock_data.current_price = stock_response['current_price']
+                stock_data.price_changed = stock_response['price_changed']
+                stock_data.percentage_changed = stock_response['percentage_changed']
+                stock_data.previous_close = stock_response['previous_close']
+                stock_data.week_52_high = stock_response['week_52_high']
+                stock_data.week_52_low = stock_response['week_52_low']
+                stock_data.market_cap = stock_response['market_cap']
+                stock_data.pe_ratio = stock_response['pe_ratio']
+                stock_data.dividend_yield = stock_response['dividend_yield']
+                stock_data.save()
+                # print('Data updated!')
+                return redirect('stock_detail', stock_data.id)
+            else:
+                messages.error(request, f'Could not fetch the data for {symbol}')
+                return redirect('stocks')
+        else:
+            print('Form is not valid')
+    else:
+        form = StockForm()
+        context = {
             'form': form,
         }
-    return render(request, 'stockanalysis/stocks.html',context)
-
+        return render(request, 'stockanalysis/stocks.html', context)
 
 class StockAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
@@ -23,4 +59,8 @@ class StockAutocomplete(autocomplete.Select2QuerySetView):
             print('result==>', qs)
 
         return qs
+    
+def stock_detail(request, pk):
+    pass
+
     
