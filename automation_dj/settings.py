126b9abc0b8e2e60f505edffc7766ae9ff96efb4
phitron-demo <sojibhasan5800@gmail.com>
2025-09-04 07:45:49 +0600

    Update email settings: added Gmail SMTP credentials and DEFAULT_FROM_EMAIL

--- automation_dj/settings.py
+++ automation_dj/settings.py
@@ -174,10 +174,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'   
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'sojibhasan5800@gmail.com'     
-# EMAIL_HOST_PASSWORD = 'vpdilgkjbkoabmta'   
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'sojibhasan5800@gmail.com'    
+EMAIL_HOST_PASSWORD = 'vpdilgkjbkoabmta'   
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
